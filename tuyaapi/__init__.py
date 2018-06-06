1fa5895cd4f4501ebe3a8571b877f1cf64e8c923
Sean Olson <seandolson654@gmail.com>
2018-06-05 19:52:27 -0700

    1

--- tuyaapi/__init__.py
+++ tuyaapi/__init__.py
@@ -7,139 +7,71 @@ import socket
 ACCESS_ID = 'nvusfxamhprcxgcnpd9n'
 ACCESS_KEY = '9dxjmakk5ar4qwwfpjdyneafcvq7hnmf'
 
-DEV_IP = '192.168.4.1'
-
 class TuyaAPI(object):
-    def __init__(self, USER_EMAIL, USER_PASSWD):
-        self.USER_EMAIL = USER_EMAIL
-        self.USER_PASSWD = USER_PASSWD
+    def __init__(self, email, passwd):
+        self.email = email
+        self.passwd = passwd
         self.sid = None
-        self.login()
+        cmd = 'tuya.m.user.email.password.login'
+        data = {
+            'countryCode': 1,
+            'email': self.email,
+            'passwd': self.passwd
+        }
+        resp = self.requestapi(cmd, data)
+        if resp == False:
+            cmd2 = 'tuya.m.user.email.register'
+            data2 = {
+                'countryCode': 1,
+                'email': self.email,
+                'passwd': self.passwd
+            }
+            resp2 = self.requestapi(cmd2, data2)
+            self.sid = resp2['sid']
+        else:
+            self.sid = resp['sid']
 
-    def requestapi(self, cmd, dataa, needsid = False):
-        accessid = ACCESS_ID
-        accesskey = ACCESS_KEY
+    def requestapi(self, cmd, dataa):
         data = json.dumps(dataa, separators=(',', ':'))
         timee = str(int(time.time()))
         h1 = hashlib.md5()
         h1.update(data.encode())
         datam = h1.hexdigest()
         datamd = datam[8:16] + datam[0:8] + datam[24:32] + datam[16:24]
-        if needsid == True:
-            signun = 'a=' + cmd + '||clientId=' + accessid + '||lang=en||os=Linux||postData=' + datamd + '||sid=' + self.sid + '||time=' + timee + '||v=1.0||' + accesskey
+        if self.sid != None:
+            signun = 'a=' + cmd + '||clientId=' + ACCESS_ID + '||lang=en||os=Linux||postData=' + datamd + '||sid=' + self.sid + '||time=' + timee + '||v=1.0||' + ACCESS_KEY
             h2 = hashlib.md5()
             h2.update(signun.encode())
             sign = h2.hexdigest()
-            url = 'https://a1.tuyaus.com/api.json?a=' + cmd + '&clientId=' + accessid + '&lang=en&os=Linux&sid=' + self.sid + '&time=' + timee + '&v=1.0&sign=' + sign
+            url = 'https://a1.tuyaus.com/api.json?a=' + cmd + '&clientId=' + ACCESS_ID + '&lang=en&os=Linux&sid=' + self.sid + '&time=' + timee + '&v=1.0&sign=' + sign
         else:
-            signun = 'a=' + cmd + '||clientId=' + accessid + '||lang=en||os=Linux||postData=' + datamd + '||time=' + timee + '||v=1.0||' + accesskey
+            signun = 'a=' + cmd + '||clientId=' + ACCESS_ID + '||lang=en||os=Linux||postData=' + datamd + '||time=' + timee + '||v=1.0||' + ACCESS_KEY
             h2 = hashlib.md5()
             h2.update(signun.encode())
             sign = h2.hexdigest()
-            url = 'https://a1.tuyaus.com/api.json?a=' + cmd + '&clientId=' + accessid + '&lang=en&os=Linux&time=' + timee + '&v=1.0&sign=' + sign
+            url = 'https://a1.tuyaus.com/api.json?a=' + cmd + '&clientId=' + ACCESS_ID + '&lang=en&os=Linux&time=' + timee + '&v=1.0&sign=' + sign
         respr = requests.post(url, data={'postData':data})
         resp = json.loads(respr.text)
-        return resp
-
-    def requestdev(self, data):
-        dataunh = json.dumps(data, separators=(',', ':'))
-        datatoenc = dataunh.encode('latin1')
-        l1 = chr(len(datatoenc) + 8).encode('latin1')
-        prefix = '\x00\x00\x55\xaa\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00'.encode('latin1')
-        suffix = '\x95\x60\xcb\x82\x00\x00\xaa\x55'.encode('latin1')
-        finaldata = prefix + l1 + datatoenc + suffix
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind(('0.0.0.0', 6670))
-        s.sendto(finaldata, (DEV_IP, 6669))
-        s.close()
-        return
-
-    def login(self):
-        cmd = 'tuya.m.user.email.password.login'
-        data = {
-            'countryCode': 1,
-            'email': self.USER_EMAIL,
-            'passwd': self.USER_PASSWD
-        }
-        resp = self.requestapi(cmd, data)
-        if resp['success'] == False:
-            cmd2 = 'tuya.m.user.email.register'
-            data2 = {
-                'countryCode': 1,
-                'email': self.USER_EMAIL,
-                'passwd': self.USER_PASSWD
-            }
-            resp2 = self.requestapi(cmd2, data2)
-            self.sid = resp2['result']['sid']
+        if resp['success'] == True:
+            respd = resp['result']
         else:
-            self.sid = resp['result']['sid']
-        return
-
-    def logout(self):
-        cmd = 'tuya.m.user.loginout'
-        data = {
-            'timeZone': '+00:00'
-        }
-        self.requestapi(cmd, data, True)
-        return
-
-    def gentok(self):
-        cmd = 'tuya.m.device.token.create'
-        data = {
-            'timeZone': '+00:00'
-        }
-        resp = self.requestapi(cmd, data, True)
-        res = {
-            'token': resp['result']['token'],
-            'secret': resp['result']['secret']
-        }
-        return res
-
-    def setupdev(self, ssid, passwd, token, secret):
-        data = {
-            'passwd': passwd,
-            'ssid': ssid,
-            'token': 'AZ' + token + secret
-        }
-        self.requestdev(data)
-        return
-
-    def ldevbytok(self, token):
-        cmd = 'tuya.m.device.list.token'
-        data = {
-            'token': token
-        }
-        resp = self.requestapi(cmd, data, True)
-        devid = resp['result'][0]['id']
-        return devid
+            respd = False
+        return respd
 
     def ldevbyuser(self):
         cmd = 'tuya.m.device.list'
         data = {}
-        resp = self.requestapi(cmd, data, True)
-        devs = []
-        for dev in resp['result']['devices']:
-            sws = len(dev['dps']) - 1
-            devs.append({'devid': dev['devId'], 'sws': sws, 'name': dev['name']})
+        resp = self.requestapi(cmd, data)
+        devs = resp['devices']
         return devs
-
-    def getsws(self, dev):
-        cmd = 'tuya.m.device.dp.get'
-        data = {
-            'devId': dev
-        }
-        resp = self.requestapi(cmd, data, True)
-        dps = resp['result']
-        sws = len(dps) - 1
-        return sws
 
     def getdevname(self, dev):
         cmd = 'tuya.m.device.get'
         data = {
             'devId': dev
         }
-        resp = self.requestapi(cmd, data, True)
-        name = resp['result']['name']
+        resp = self.requestapi(cmd, data)
+        name = resp['name']
         return name
 
     def setdevname(self, dev, name):
@@ -148,40 +80,66 @@ class TuyaAPI(object):
             'devId': dev,
             'name': name
         }
-        self.requestapi(cmd, data, True)
+        self.requestapi(cmd, data)
         return
 
-    def getdps(self, dev):
-        cmd = 'tuya.m.device.dp.get'
-        data = {
-            'devId': dev
-        }
-        resp = self.requestapi(cmd, data, True)
-        dps = resp['result']
-        dpss = {}
-        i = 1
-        for key, value in dps.items():
-            if i != len(dps):
-                dpss[key] = value
-                i = i + 1
-        return dpss
+class DevSetup(object):
+    def __init__(self, tapi, ssid, passw):
+        self.tapi = tapi
+        self.ssid = ssid
+        self.passw = passw
+        self.token = None
+        self.secret = None
 
-    def setdps(self, dev, dps):
-        cmd = 'tuya.m.device.dp.publish'
+    def gentok(self):
+        cmd = 'tuya.m.device.token.create'
         data = {
-            'devId': dev,
-            'dps': dps
+            'timeZone': '+00:00'
         }
-        self.requestapi(cmd, data, True)
+        resp = self.tapi.requestapi(cmd, data)
+        self.token = resp['token']
+        self.secret = resp['secret']
         return
 
-class TuyaDevice(object):
+    def setupdev(self):
+        data = {
+            'passwd': self.passw,
+            'ssid': self.ssid,
+            'token': 'AZ' + self.token + self.secret
+        }
+        dataunh = json.dumps(data, separators=(',', ':'))
+        datatoenc = dataunh.encode('latin1')
+        l1 = chr(len(datatoenc) + 8).encode('latin1')
+        prefix = '\x00\x00\x55\xaa\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00'.encode('latin1')
+        suffix = '\x95\x60\xcb\x82\x00\x00\xaa\x55'.encode('latin1')
+        finaldata = prefix + l1 + datatoenc + suffix
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(('0.0.0.0', 6670))
+        s.sendto(finaldata, ('192.168.1.4', 6669))
+        s.close()
+        return
+
+    def ldevbytok(self):
+        cmd = 'tuya.m.device.list.token'
+        data = {
+            'token': self.token
+        }
+        resp = self.tapi.requestapi(cmd, data)
+        devid = resp[0]['id']
+        return devid
+
+class TuyaSwitch(object):
     def __init__(self, tapi, dev):
         self.tapi = tapi
         self.dev = dev
 
     def getsws(self):
-        sws = self.tapi.getsws(self.dev)
+        cmd = 'tuya.m.device.dp.get'
+        data = {
+            'devId': self.dev
+        }
+        resp = self.tapi.requestapi(cmd, data)
+        sws = len(resp) - 1
         return sws
 
     def getname(self):
@@ -193,11 +151,20 @@ class TuyaDevice(object):
         return
 
     def getstate(self, swid = 1):
-        dps = self.tapi.getdps(self.dev)
-        state = dps[str(swid)]
-        return state
+        cmd = 'tuya.m.device.dp.get'
+        data = {
+            'devId': self.dev
+        }
+        resp = self.tapi.requestapi(cmd, data)
+        return resp[str(swid)]
 
     def setstate(self, state, swid = 1):
-        dps = {str(swid): state}
-        self.tapi.setdps(self.dev, dps)
+        cmd = 'tuya.m.device.dp.publish'
+        data = {
+            'devId': self.dev,
+            'dps': {
+                str(swid): state
+            }
+        }
+        self.tapi.requestapi(cmd, data)
         return
